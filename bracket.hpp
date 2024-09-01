cc7d2d3cf4f5e78d93f95336b4ac8df8678b372b
rachdy <ismailrchdy123@gmail.com>
2024-09-01 16:29:55 +0200

    i added some changes on Header Bracket file

--- bracket.hpp
+++ bracket.hpp
@@ -1,13 +1,16 @@
+#ifndef _Bracket_HPP
 
-
+#define _Bracket_HPP
 
 #include<iostream>
 #include<string>
 #include<ctime>
 #include<vector>
 
-typedef struct Stack;
+struct Stack;
 bool pair(char open , char close);
 bool check(std::string str);
 std::string str_generate();
 void test();
+
+#endif 
